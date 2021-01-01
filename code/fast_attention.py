21b5cb5d3ab4913ff42937a6f10f7d9dd27df157
Justin Chiu <justinchiu@g.harvard.edu>
2020-12-31 22:05:20 -0500

    compare torch and jax

--- code/fast_attention.py
+++ code/fast_attention.py
@@ -13,24 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Core Fast Attention Module for Flax.
-
-Implementation of the approximate fast softmax and generalized
-attention mechanism leveraging structured random feature maps [RFM] techniques
-and low rank decomposition of the attention matrix.
-"""
 # pylint: disable=invalid-name, missing-function-docstring, line-too-long
 
 import abc
 from collections.abc import Iterable  # pylint: disable=g-importing-member
 import functools
-#from absl import logging
 import logging
-#import gin
 import math
-
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 import jax
 from jax import lax
@@ -41,15 +30,59 @@ from jax.scipy.special import logsumexp as lse
 
 import numpy as onp
 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
 import streamlit as st
 
-#onp.set_printoptions(precision=2)
 onp.set_printoptions(suppress=True, precision=2)
 
 def nonnegative_softmax_kernel_feature_creator(
     data,
     projection_matrix,
-    attention_dims_t,
+    is_query,
+    normalize_data=True,
+    eps=0.0001,
+):
+    """Constructs nonnegative kernel features for fast softmax attention.
+
+
+    Args:
+    data: input for which features are computes
+    projection_matrix: random matrix used to compute features
+    batch_dims_t: tuple of batch dimensions
+    is_query: predicate indicating whether input data corresponds to queries or
+      keys
+    normalize_data: predicate indicating whether data should be normalized,
+    eps: numerical stabilizer.
+
+    Returns:
+    Random features for fast softmax attention.
+    """
+    # We have e^{qk^T/sqrt{d}} = e^{q_norm k_norm^T} (transformer), where
+    # w_norm = w * data_normalizer for w in {q,k}.
+    data_normalizer = 1.0 / (jnp.sqrt(jnp.sqrt(data.shape[-1]))) if normalize_data else 1.0
+
+    # ratio is for computing the empirical mean / avg
+    # its a constant that gets cancelled out in softmax though?
+    ratio = 1.0 / jnp.sqrt(projection_matrix.shape[0])
+    #ratio = 1.0
+    #data_mod_shape = data.shape[0:len(batch_dims_t)] + projection_matrix.shape
+    #data_thick_random_matrix = jnp.zeros(data_mod_shape) + projection_matrix
+
+    # matmul / linear projection
+    data_dash = jnp.einsum("...bd,...fd->...bf", data_normalizer * data, projection_matrix)
+
+    diag_data = jnp.square(data)
+    diag_data = jnp.sum(diag_data, axis=data.ndim - 1)
+    diag_data = (diag_data / 2.0) * data_normalizer * data_normalizer
+    diag_data = jnp.expand_dims(diag_data, axis=data.ndim - 1)
+
+    return data_dash - diag_data + eps - math.log(ratio)
+
+def nonnegative_softmax_kernel_feature_creator0(
+    data,
+    projection_matrix,
     batch_dims_t,
     precision,
     is_query,
@@ -58,11 +91,9 @@ def nonnegative_softmax_kernel_feature_creator(
 ):
   """Constructs nonnegative kernel features for fast softmax attention.
 
-
   Args:
     data: input for which features are computes
     projection_matrix: random matrix used to compute features
-    attention_dims_t: tuple of attention dimensions
     batch_dims_t: tuple of batch dimensions
     precision: precision parameter
     is_query: predicate indicating whether input data corresponds to queries or
@@ -73,7 +104,6 @@ def nonnegative_softmax_kernel_feature_creator(
   Returns:
     Random features for fast softmax attention.
   """
-  del attention_dims_t
   if normalize_data:
     # We have e^{qk^T/sqrt{d}} = e^{q_norm k_norm^T}, where
     # w_norm = w * data_normalizer for w in {q,k}.
@@ -100,9 +130,9 @@ def nonnegative_softmax_kernel_feature_creator(
   diag_data = (diag_data / 2.0) * data_normalizer * data_normalizer
   diag_data = jnp.expand_dims(diag_data, axis=data.ndim - 1)
 
-  return data_dash - diag_data 
+  #return data_dash - diag_data 
 
-  """
+  #"""
   if is_query:
     last_dims_t = (len(data_dash.shape) - 1,)
     data_dash = ratio * (
@@ -113,8 +143,9 @@ def nonnegative_softmax_kernel_feature_creator(
         jnp.exp(data_dash - diag_data - jnp.max(data_dash)) + eps)
 
   return data_dash
-  """
+  #"""
 
+# garbage
 class RandomMatrix(object):
   r"""Abstract class providing a method for constructing 2D random arrays.
 
@@ -185,6 +216,8 @@ class GaussianOrthogonalRandomMatrix(RandomMatrix):
 
     return jnp.matmul(jnp.diag(multiplier), final_matrix)
 
+# /garbage
+
 def get_2d_array(unstructured_blocks, key, scaling=0):
     nb_rows, nb_columns = unstructured_blocks.shape
     nb_full_blocks = int(nb_rows / nb_columns)
@@ -192,19 +225,6 @@ def get_2d_array(unstructured_blocks, key, scaling=0):
     remaining_rows = nb_rows - nb_full_blocks * nb_columns
     if remaining_rows > 0:
         raise ValueError("Assert nb_rows % nb_columns == 0 for simplicity")
-
-    """
-    block_list = []
-    for i in range(nb_full_blocks):
-        start = i * nb_columns
-        end = (i+1) * nb_columns
-        unstructured_block = unstructured_blocks[start:end]
-        q, _ = jnp.linalg.qr(unstructured_block)
-        q = jnp.transpose(q)
-        block_list.append(q)
-
-    final_matrix = jnp.vstack(block_list)
-    """
 
     blocks = unstructured_blocks.reshape((nb_full_blocks, nb_columns, nb_columns))
     Q, _ = jnp.linalg.qr(blocks)
@@ -243,9 +263,9 @@ lmm = jax.jit(lambda x,y: lse(x[:,None,:] + y[None,:,:], -1))
 def rff_attn(q, k, projection_matrix):
     kernel_cons = nonnegative_softmax_kernel_feature_creator
     log_phi_q = kernel_cons(
-        q, projection_matrix, None, (0,), None, is_query=True, eps=0, normalize_data=True)
+        q, projection_matrix, is_query=True, eps=0, normalize_data=True)
     log_phi_k = kernel_cons(
-        k, projection_matrix, None, (0,), None, is_query=False, eps=0, normalize_data=True)
+        k, projection_matrix, is_query=False, eps=0, normalize_data=True)
     log_pots_hat = log_phi_q[:,None,:] + log_phi_k[None,:,:]
     # average
     log_pots = lmm(log_phi_q, log_phi_k) - math.log(k.shape[0])
@@ -253,172 +273,174 @@ def rff_attn(q, k, projection_matrix):
 
 rffa = jax.jit(rff_attn)
 
+def rff_attn0(q, k, projection_matrix):
+    kernel_cons = nonnegative_softmax_kernel_feature_creator0
+    phi_q = kernel_cons(
+        q, projection_matrix, (0,), None, is_query=True, eps=0, normalize_data=True)
+    phi_k = kernel_cons(
+        k, projection_matrix, (0,), None, is_query=False, eps=0, normalize_data=True)
+    uprobs = phi_q @ phi_k.T
+    return uprobs / uprobs.sum(-1, keepdims=True)
+
+rffa0 = jax.jit(rff_attn0)
+
 def kl(p, q):
     e_ratio = p * (jnp.log(p) - jnp.log(q))
     #e_ratio = jax.ops.index_update(e_ratio, p == 0, 0)
     return e_ratio.sum(-1)
 
-key = jax.random.PRNGKey(0)
+if __name__ == "__main__":
+    key = jax.random.PRNGKey(0)
 
-key, key1, key2 = jax.random.split(key, 3)
-qk_dim = 8
-T = 4
-q = jax.random.normal(key1, (T, qk_dim)) 
-k = jax.random.normal(key2, (T, qk_dim))
-qu, ku = q.copy(), k.copy()
-qs, ks = q.copy(), k.copy()
+    key, key1, key2 = jax.random.split(key, 3)
+    qk_dim = 8
+    T = 4
+    q = jax.random.normal(key1, (T, qk_dim)) 
+    k = jax.random.normal(key2, (T, qk_dim))
+    qu, ku = q.copy(), k.copy()
+    qs, ks = q.copy(), k.copy()
 
-num_features = 256
-#num_features = 32000
+    num_features = 256
+    #num_features = 32000
 
-st.markdown("## Categorical approximation")
-st.write("We start by trying to approximate a small-dimensional categorical distribution.")
+    st.markdown("## Categorical approximation")
+    st.write("We start by trying to approximate a small-dimensional categorical distribution.")
 
-st.write("We use the RFF approximation with")
-st.write(f"num_features = {num_features}")
+    st.write("We use the RFF approximation with")
+    st.write(f"num_features = {num_features}")
 
-unstructured = GaussianUnstructuredRandomMatrix(num_features, qk_dim, key).get_2d_array()
-structured = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key).get_2d_array()
+    unstructured = GaussianUnstructuredRandomMatrix(num_features, qk_dim, key).get_2d_array()
+    structured = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key).get_2d_array()
 
-attn_dist, logits = attn(q, k)
-#projection_matrix = random.normal(key, (num_features, qk_dim))
-projection_matrix = random_projection(num_features, qk_dim, key)
-rff_unstruct_attn_dist, unstruct_logits_hat = rff_attn(qu, ku, projection_matrix)
-projection_matrix0 = structured
-rff_struct_attn_dist0, struct_logits_hat0 = rff_attn(qs, ks, projection_matrix0)
-projection_matrix = get_2d_array(projection_matrix, key)
-rff_struct_attn_dist, struct_logits_hat = rff_attn(qs, ks, projection_matrix)
+    attn_dist, logits = attn(q, k)
+    #projection_matrix = random.normal(key, (num_features, qk_dim))
+    projection_matrix = random_projection(num_features, qk_dim, key)
+    rff_unstruct_attn_dist, unstruct_logits_hat = rff_attn(qu, ku, projection_matrix)
 
-_, key1 = random.split(key)
-projection_matrix1 = get_2d_array(projection_matrix, key1)
-rff_struct_attn_dist1, struct_logits_hat1 = rff_attn(qs, ks, projection_matrix1)
-
-def write(x):
-    st.dataframe(jnp.asarray(x))
-
-st.write("Given a set of query and key embeddings, we compare the RFF approximation to the true distributions")
-st.write("kl(attn, unstruct_attn)")
-write(kl(attn_dist, rff_unstruct_attn_dist))
-st.write("kl(attn, struct_attn)")
-write(kl(attn_dist, rff_struct_attn_dist))
-st.write("kl(attn, struct_attn0)")
-write(kl(attn_dist, rff_struct_attn_dist0))
-
-st.write("The exponentiated logits, which the RFF approximates")
-st.write("attn exp(logits)")
-write(jnp.exp(logits))
-st.write("unstruct attn exp(logits)")
-write(jnp.exp(unstruct_logits_hat).mean(-1))
-st.write("struct attn exp(logits)")
-write(jnp.exp(struct_logits_hat).mean(-1))
-st.write("struct attn exp(logits0)")
-write(jnp.exp(struct_logits_hat0).mean(-1))
-
-st.write("We also measure variance in a given exp kernel approximation")
-st.write("unstruct attn var exp(logits)")
-write(jnp.exp(unstruct_logits_hat).var(-1))
-st.write("struct attn var exp(logits)")
-write(jnp.exp(struct_logits_hat).var(-1))
-st.write("struct attn var exp(logits0)")
-write(jnp.exp(struct_logits_hat0).var(-1))
+    rff_unstruct_attn_dist0 = rff_attn0(qu, ku, projection_matrix)
 
 
-# check mean and variance of original implementation
-NUM_ITERS = 100
-attn_dist, logits = attn(q, k)
-attns = []
-attns0 = []
-for i in range(NUM_ITERS):
-    key, key1 = random.split(key)
-    structured = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
+    _, newkey = jax.random.split(key)
+    samples = []
+    samples0 = []
+    for i in range(100):
+        newkey, key1 = jax.random.split(newkey)
+        unstructured = GaussianUnstructuredRandomMatrix(num_features, qk_dim, key1).get_2d_array()
+        a = rff_attn0(qu, ku, unstructured)
+        b, _ = rff_attn(qu, ku, unstructured)
+        samples0.append(a)
+        samples.append(b)
+    samples = onp.stack(samples)
+    samples0 = onp.stack(samples0)
+    #import pdb; pdb.set_trace()
 
     projection_matrix0 = structured
-    rff_struct_attn_dist0, _ = rffa(qs, ks, projection_matrix0)
-    projection_matrix = get_2d_array(projection_matrix, key1)
-    rff_struct_attn_dist, _ = rffa(qs, ks, projection_matrix)
+    rff_struct_attn_dist0, struct_logits_hat0 = rff_attn(qs, ks, projection_matrix0)
+    projection_matrix = get_2d_array(projection_matrix, key)
+    rff_struct_attn_dist, struct_logits_hat = rff_attn(qs, ks, projection_matrix)
 
-    attns.append(onp.array(rff_struct_attn_dist))
-    attns0.append(onp.array(rff_struct_attn_dist0))
+    _, key1 = random.split(key)
+    projection_matrix1 = get_2d_array(projection_matrix, key1)
+    rff_struct_attn_dist1, struct_logits_hat1 = rff_attn(qs, ks, projection_matrix1)
 
-attns = onp.stack(attns)
-attns0 = onp.stack(attns0)
-import pdb; pdb.set_trace()
+    def write(x):
+        st.dataframe(jnp.asarray(x))
 
-st.markdown("## Learning an RFF Cat")
-# can we optimize to minimize variance + KL?
-def loss_u(q, k, attn_dist, projection_matrix):
-    rff_unstruct_attn_dist, _ = rff_attn(q, k, projection_matrix)
-    return kl(attn_dist, rff_unstruct_attn_dist).mean()
+    st.write("Given a set of query and key embeddings, we compare the RFF approximation to the true distributions")
+    st.write("kl(attn, unstruct_attn)")
+    write(kl(attn_dist, rff_unstruct_attn_dist))
+    st.write("kl(attn, struct_attn)")
+    write(kl(attn_dist, rff_struct_attn_dist))
+    st.write("kl(attn, struct_attn0)")
+    write(kl(attn_dist, rff_struct_attn_dist0))
 
-L = jax.jit(jax.value_and_grad(loss_u, argnums=(0, 1)))
+    st.write("The exponentiated logits, which the RFF approximates")
+    st.write("attn exp(logits)")
+    write(jnp.exp(logits))
+    st.write("unstruct attn exp(logits)")
+    write(jnp.exp(unstruct_logits_hat).mean(-1))
+    st.write("struct attn exp(logits)")
+    write(jnp.exp(struct_logits_hat).mean(-1))
+    st.write("struct attn exp(logits0)")
+    write(jnp.exp(struct_logits_hat0).mean(-1))
 
-"""
-NUM_ITERS = 1000
-alpha = 1e-2
-losses = onp.zeros((NUM_ITERS,))
-for i in range(NUM_ITERS):
+    st.write("We also measure variance in a given exp kernel approximation")
+    st.write("unstruct attn var exp(logits)")
+    write(jnp.exp(unstruct_logits_hat).var(-1))
+    st.write("struct attn var exp(logits)")
+    write(jnp.exp(struct_logits_hat).var(-1))
+    st.write("struct attn var exp(logits0)")
+    write(jnp.exp(struct_logits_hat0).var(-1))
+
+
+    # check mean and variance of original implementation
+    NUM_ITERS = 100
+    attn_dist, logits = attn(q, k)
+    attns = []
+    attns0 = []
+    for i in range(NUM_ITERS):
+        key, key1 = random.split(key)
+        structured = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
+
+        projection_matrix0 = structured
+        rff_struct_attn_dist0, _ = rffa(qs, ks, projection_matrix0)
+        projection_matrix = get_2d_array(projection_matrix, key1)
+        rff_struct_attn_dist, _ = rffa(qs, ks, projection_matrix)
+
+        attns.append(onp.array(rff_struct_attn_dist))
+        attns0.append(onp.array(rff_struct_attn_dist0))
+
+    attns = onp.stack(attns)
+    attns0 = onp.stack(attns0)
+    import pdb; pdb.set_trace()
+
+    st.markdown("## Learning an RFF Cat")
+    # can we optimize to minimize variance + KL?
+    def loss_u(q, k, attn_dist, projection_matrix):
+        rff_unstruct_attn_dist, _ = rff_attn(q, k, projection_matrix)
+        return kl(attn_dist, rff_unstruct_attn_dist).mean()
+
+    L = jax.jit(jax.value_and_grad(loss_u, argnums=(0, 1)))
+
+    """
+    NUM_ITERS = 1000
+    alpha = 1e-2
+    losses = onp.zeros((NUM_ITERS,))
+    for i in range(NUM_ITERS):
+        key, key1 = jax.random.split(key, 2)
+        unstructured_blocks = random.normal(key1, (num_features, qk_dim))
+        projection_matrix = get_2d_array(unstructured_blocks, key1)
+        projection_matrix0 = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
+        #import pdb; pdb.set_trace()
+        kl_val, (dq, dk) = L(qu, ku, attn_dist, projection_matrix)
+        qu -= alpha * dq
+        ku -= alpha * dk
+        losses[i] = kl_val
+
+    fig = go.Figure(data=go.Scatter(
+        x = onp.arange(NUM_ITERS),
+        y = losses,
+        mode = "markers",
+    ))
+    st.plotly_chart(fig, use_container_width=True)
+    """
+
+
+    # get covariance
+    N = 2
     key, key1 = jax.random.split(key, 2)
-    unstructured_blocks = random.normal(key1, (num_features, qk_dim))
-    projection_matrix = get_2d_array(unstructured_blocks, key1)
-    projection_matrix0 = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
-    #import pdb; pdb.set_trace()
-    kl_val, (dq, dk) = L(qu, ku, attn_dist, projection_matrix)
-    qu -= alpha * dq
-    ku -= alpha * dk
-    losses[i] = kl_val
+    unstructured_blocks = random_projection(num_features, qk_dim, key1, bsz=N)
+    keys = jax.random.split(key, N+1)
+    key = keys[0]
+    projection_matrices = get_2d_arrays(unstructured_blocks, keys[1:])
+    rff_attns = jax.jit(jax.vmap(rff_attn, in_axes=(None,None,0))) # only broadcast projection_matrices
 
-fig = go.Figure(data=go.Scatter(
-    x = onp.arange(NUM_ITERS),
-    y = losses,
-    mode = "markers",
-))
-st.plotly_chart(fig, use_container_width=True)
-"""
+    attn_dists, logits_hat = rff_attns(qu, ku, projection_matrices)
 
-
-# get covariance
-N = 2
-key, key1 = jax.random.split(key, 2)
-unstructured_blocks = random_projection(num_features, qk_dim, key1, bsz=N)
-keys = jax.random.split(key, N+1)
-key = keys[0]
-projection_matrices = get_2d_arrays(unstructured_blocks, keys[1:])
-rff_attns = jax.jit(jax.vmap(rff_attn, in_axes=(None,None,0))) # only broadcast projection_matrices
-
-attn_dists, logits_hat = rff_attns(qu, ku, projection_matrices)
-
-# debug
-#print(jnp.isnan(projection_matrices).any())
-#print(jnp.isnan(projection_matrices).nonzero())
-
-"""
-print(jnp.isnan(attn_dists).any())
-print(jnp.isnan(attn_dists).nonzero())
-
-projection_matrix = projection_matrices[16]
-
-kernel_cons = nonnegative_softmax_kernel_feature_creator
-log_phi_q = kernel_cons(
-    qu, projection_matrix, None, (0,), None, is_query=True, eps=1e-8, normalize_data=False)
-log_phi_k = kernel_cons(
-    ku, projection_matrix, None, (0,), None, is_query=False, eps=1e-8, normalize_data=False)
-log_pots_hat = log_phi_q[:,None,:] + log_phi_k[None,:,:]
-log_pots = lmm(log_phi_q, log_phi_k)
-adist = jnp.exp(log_pots - lse(log_pots, -1, keepdims=True))
-
-print(jnp.isnan(log_phi_q).any())
-print(jnp.isnan(log_phi_k).any())
-print(jnp.isnan(log_phi_q).nonzero())
-print(jnp.isnan(log_phi_k).nonzero())
-"""
-
-import pdb; pdb.set_trace()
-# /debug
-
-for i in range(5):
-    key, key1 = jax.random.split(key, 2)
-    projection_matrix = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
-    rff_attn_dist, logits_hat = rff_attn(qu, ku, projection_matrix)
-    print(rff_attn_dist)
-    print(jnp.exp(logits_hat).var(-1))
-    #import pdb; pdb.set_trace()
+    for i in range(5):
+        key, key1 = jax.random.split(key, 2)
+        projection_matrix = GaussianOrthogonalRandomMatrix(num_features, qk_dim, key1).get_2d_array()
+        rff_attn_dist, logits_hat = rff_attn(qu, ku, projection_matrix)
+        print(rff_attn_dist)
+        print(jnp.exp(logits_hat).var(-1))
+        #import pdb; pdb.set_trace()
